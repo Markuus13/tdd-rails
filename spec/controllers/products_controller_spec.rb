421b8c48936eade97bd908117b923049b390bd61
Marcus Vinícius <marcus.f22@gmail.com>
2017-02-12 22:39:58 -0200

    GET ProductController#index test - refactor

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
-class ProductsController < ApplicationController
-  def index
-    @products = Product.all
-  end
-end
-
 RSpec.describe ProductsController, type: :controller do
   describe 'GET #index' do
     it 'assigns @products' do
