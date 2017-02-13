b1e2ddd20fbf311d34d9ba04b31b9dffd6c7bb67
Marcus Vinícius <marcus.f22@gmail.com>
2017-02-13 19:55:45 -0200

    Rspec ProductsController GET#index refactored

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   describe 'GET #index' do
-    it 'assigns @products' do
+    it 'assigns all products to @products' do
       product = Product.create
       get :index
       expect(assigns(:products)).to eq([product])
     end
   end
-
 end
